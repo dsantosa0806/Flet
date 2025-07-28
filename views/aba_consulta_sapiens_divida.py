028e626119d205ceda5c1d1ac13d11fe309d771c
Daniel Almeida <93289530+dsantosa0806@users.noreply.github.com>
2025-07-28 00:04:38 -0300

    Ajustes 28.07

--- views/aba_consulta_sapiens_divida.py
+++ views/aba_consulta_sapiens_divida.py
@@ -259,7 +259,7 @@ def aba_consulta_sapiens(ft, DEFAULT_FONT_SIZE, HEADING_FONT_SIZE, page):
     container_tabela_doc = ft.Container(
         content=table_doc,
         expand=True,
-        height=1100,
+        height=1200,
         padding=10,
         border_radius=10,
         border=ft.border.all(1, ft.Colors.GREY_600),
